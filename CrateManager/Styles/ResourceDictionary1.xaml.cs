47f4cff299d452d3082f5cf4eb76f89c30b05714
sillycrackers <westervelderik@gmail.com>
2021-12-14 13:20:56 -0500

    first commit

---
+++ CrateManager/Styles/ResourceDictionary1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrateManager.Styles
+{
+    partial class ResourceDictionary1 : ResourceDictionary
+    {
+        public ResourceDictionary1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
