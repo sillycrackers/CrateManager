87c3243bbc6b58f5e76e8145455547935eac5edd
sillycrackers <westervelderik@gmail.com>
2021-12-16 16:06:45 -0500

    added gitignore

--- CrateManager/obj/Debug/net5.0-windows/App.g.cs
+++ CrateManager/obj/Debug/net5.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace CrateManager {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace CrateManager {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public static void Main() {
             CrateManager.App app = new CrateManager.App();
             app.InitializeComponent();
